c828694c7c5a19fce05ab56cae73f1aea8c86c95
jingming295 <jingming295@gmail.com>
2023-10-29 23:24:41 +0800

    🔨modified: index.ts

--- src/API/Database/select-database.ts
+++ src/API/Database/select-database.ts
@@ -2,6 +2,7 @@ import { CreateDatabase } from './create-database'
 
 export class Select extends CreateDatabase{
     async select() {
+      console.log(this.ctx.database)
       const data = await this.ctx.database.get('BilibiliAccount', {
         id: 1
       })
--- src/API/index.ts
+++ src/API/index.ts
@@ -1,15 +1,20 @@
 import { Context, Logger } from "koishi";
-import { BiliBiliApi } from "./BiliBiliAPI";
 import { Select } from "./Database/select-database";
 import { BilibiliAccount } from "./BilibiliAccount";
+import { CreateDatabase } from "./Database/create-database";
 
 export async function apply(ctx: Context, Config: Config)
 {
   const logger = new Logger('bilibili-login');
+  // 不等待的话，database好像还没初始化
+  await new Promise(resolve => setTimeout(resolve, 500)); // 等待2秒
   try
   {
+    const createDatabase = new CreateDatabase(ctx);
     const select = new Select(ctx);
     const bilibiliAccount = new BilibiliAccount(ctx);
+
+    createDatabase.CreateBilibiliAccountDatabase()
     const BilibiliAccountData = await select.select();
     if (BilibiliAccountData.length !== 1) bilibiliAccount.init(Config);
     else
